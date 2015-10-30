40f94ddf0770a3969acd8178b574dc296c637caf
alu0100309553 <alu0100309553@ull.edu.es>
2015-10-30 11:07:04 +0000

    Implementadas operaciones de resta div y mult y pruebas

--- lib/fraccionarios.rb
+++ lib/fraccionarios.rb
@@ -1,21 +1,54 @@
 class Fraccionarios
+    
+    #inicializador de la clase
     def initialize (num, den)
         @num=num
         @den=den
     end
+    
+    #retorna el valor del denominador
     def den
         return @den
     end
-    def num
+    
+    #retorna el valor del numerador
+    def num 
         return @num
     end
-    def string()
+    
+    #retorna la fraccion en forma de string
+    def string
         return "#{@num}/#{@den}"
     end
     
+    #suma
     def sum(fraccion)
         n=(@num*fraccion.den)+(fraccion.num*@den)
         d=@den*fraccion.den
+        r=Fraccionarios.new(n,d)
+        return r.string
+    end
+    
+    #resta
+    def res(fraccion)
+        n=(@num*fraccion.den)-(fraccion.num*@den)
+        d=@den*fraccion.den
+        r=Fraccionarios.new(n,d)
+        return r.string
+    end
+    
+    #multiplicación
+    def mul(fraccion)
+        n=(@num*fraccion.num)
+        d=@den*fraccion.den
+        r=Fraccionarios.new(n,d)
+        return r.string
+    end
+    
+    #división
+    def div(fraccion)
+        n=(@num*fraccion.den)
+        d=@den*fraccion.num
         r=Fraccionarios.new(n,d)
         return r.string
     end
--- lib/main.rb
+++ lib/main.rb
@@ -3,3 +3,6 @@ require "./fraccionarios.rb"
 a=Fraccionarios.new(1,2)
 b=Fraccionarios.new(3,4)
 puts a.sum(b)
+puts a.res(b)
+puts a.mul(b)
+puts a.div(b)

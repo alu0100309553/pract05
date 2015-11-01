7db3224e76fb83481354f6725e7eec8bd0adb662
alu0100309553 <alu0100309553@ull.edu.es>
2015-11-01 11:33:35 +0000

    Corrigido test

--- lib/tc_fraccionarios.rb
+++ lib/tc_fraccionarios.rb
@@ -10,19 +10,19 @@ class Test_Fraccionarios < Test::Unit::TestCase
    end
    
    def test_suma
-      assert_equal("52/21", @n1.sum(@n2).string)
+      assert_equal("52/21", (@n1 + @n2).to_s)
    end                   
    
    def test_resta
-      assert_equal("-4/21", @n1.res(@n2).string)
+      assert_equal("-4/21", (@n1 - @n2).to_s)
    end  
    
    def test_multi
-      assert_equal("32/21", @n1.mul(@n2).string)
+      assert_equal("32/21", (@n1 * @n2).to_s)
    end
    
    def test_divi
-      assert_equal("24/28", @n1.div(@n2).string)
+      assert_equal("24/28", (@n1 / @n2).to_s)
    end
    
 end
